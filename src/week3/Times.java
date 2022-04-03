7762845432d678f8dc27e9ba1989ef1a9973a880
Chris-Ru <72892271+Chris-Ru@users.noreply.github.com>
2022-04-03 16:43:43 -0700

    add missing code

--- src/week3/Times.java
+++ src/week3/Times.java
@@ -19,9 +19,11 @@ public class Times {
         }
         // use Inheritance and Polymorphism to replace data.sort with your own algorithm
         // data.sort(Comparator.naturalOrder());
-        // MergeSort.sort(data);
+        BubbleSort.sort(data);
         Insertion.sort(data);
-        
+        MergeSort.sort(data);
+        Selection.sort(data);
+
         Instant end = Instant.now();    // time capture -- end
         this.timeElapsed = Duration.between(start, end);
     }
@@ -34,24 +36,24 @@ public class Times {
         return timeElapsed.getNano();
     }
 
-  public static void main(String[] args) {
-      int sum=0, time=0, TIMES=12, SIZE=5000;
+    public static void main(String[] args) {
+        int sum=0, time=0, TIMES=12, SIZE=5000;
 
-      for(int i=0; i< TIMES; i++) {
-          Times s = new Times(SIZE);
-          for(int j = 0; j<s.getData().size(); j++) {
-              // To see data, uncomment next line
-              // System.out.println(s.getData());
-              sum += s.getData().get(j);
-          }
-          System.out.println("Average random: " + sum / ((i+1)*SIZE));
-          System.out.println("Nanoseconds: " + s.getTimeElapsed());
-          time += s.getTimeElapsed();
-      }
-      System.out.println("Average random: " + sum / (TIMES*SIZE));
-      System.out.println("Total Nanoseconds: " + time );
-      System.out.println("Total Seconds: " + time /1000000000.0);
+        for(int i=0; i< TIMES; i++) {
+            Times s = new Times(SIZE);
+            for(int j = 0; j<s.getData().size(); j++) {
+                // To see data, uncomment next line
+                // System.out.println(s.getData());
+                sum += s.getData().get(j);
+            }
+            System.out.println("Average random: " + sum / ((i+1)*SIZE));
+            System.out.println("Nanoseconds: " + s.getTimeElapsed());
+            time += s.getTimeElapsed();
+        }
+        System.out.println("Average random: " + sum / (TIMES*SIZE));
+        System.out.println("Total Nanoseconds: " + time );
+        System.out.println("Total Seconds: " + time /1000000000.0);
 
-      System.out.println(" ");
+        System.out.println(" ");
     }
 }
