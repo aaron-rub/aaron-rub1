91ae11114fd81c5bfd7eafc4b8468b01d787ea7f
Chris-Ru <72892271+Chris-Ru@users.noreply.github.com>
2022-04-03 16:35:43 -0700

    code proper selection sort without timing in class to reduce clutter

--- src/week3/sorts/Selection.java
+++ src/week3/sorts/Selection.java
@@ -10,63 +10,48 @@ import java.util.Comparator;
 
 public class Selection {
 
-    private final ArrayList<Integer> data = new ArrayList<>();
-    private final Duration timeElapsed;
+    private ArrayList<Integer> inputArray = new ArrayList<Integer>();
 
-    public Selection(int size) {
-        Instant start = Instant.now();  // time capture -- start
-        // build an array
-        for (int i = 0; i < size; i++) {
-            data.add((int)(Math.random() * (size+1)));
-        }
-        // use Inheritance and Polymorphism to replace data.sort with your own algorithm
-        data.sort(Comparator.naturalOrder());
-        Instant end = Instant.now();    // time capture -- end
-        this.timeElapsed = Duration.between(start, end);
+    //Just To fetch display purpose
+    public ArrayList<Integer> getSortedArray() {
+        return inputArray;
     }
 
-    public ArrayList<Integer> getData() {
-        return data;
+    public Selection(ArrayList<Integer> inputArray){
+        this.inputArray = inputArray;
     }
 
-    public int getTimeElapsed() {
-        return timeElapsed.getNano();
-    }
+    public void sortGivenArray(){
 
-    // Reads in an array of integers, sorts them,
-    // then prints them in sorted order.
-    public static void main(String[] args){
-        int sum=0, time=0, TIMES=12, SIZE=5000;
+        int smallInt = 0;
+        int j=0;
+        int smallIntIndex = 0;
 
-        for(int n=0; n< TIMES; n++) {
-            Times s = new Times(SIZE);
-            for(int j = 0; j<s.getData().size(); j++) {
-                // To see data, uncomment next line
-                // System.out.println(s.getData());
-                sum += s.getData().get(j);
+        for(int i=1;i<inputArray.size();i++){
 
-                System.out.println("\nAverage random: " + sum / ((n+1)*SIZE));
-                System.out.println("\nNanoseconds: " + s.getTimeElapsed());
-                time += s.getTimeElapsed();
+            smallInt = inputArray.get(i-1);
+            smallIntIndex = i-1;
 
-
-
-                Integer[] intList;
-
-                System.out.println("\nSelection Sort");
-
-                intList = new Integer[TIMES];
-
-                for (int i = 0; i < TIMES; i++)
-                    intList[i] = sum;
-                Sorting.selectionSort(intList);
-
-                System.out.println("\nYour numbers in sorted order...");
-
-                for (int i = 0; i < TIMES; i++)
-                    System.out.print(intList[i] + " ");
-                System.out.println();
+            for(j=i;j<inputArray.size();j++){
+                if(inputArray.get(j)<smallInt){
+                    smallInt = inputArray.get(j);
+                    smallIntIndex = j;
+                }
             }
+
+            //Swap the smallest element with the first element of unsorted subarray
+            swap(i-1, smallIntIndex);
         }
+    }
+
+    public void swap(int sourceIndex,int destIndex){
+        int temp = inputArray.get(destIndex);
+        inputArray.set(destIndex, inputArray.get(sourceIndex));
+        inputArray.set(sourceIndex, temp);
+    }
+
+
+    public static void sort(ArrayList<Integer> a) {
+        Selection s = new Selection(a);
     }
 }
