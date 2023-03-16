7fc1277f046bc23eeb2d6865020c4ee02ef8f8ec
aaron-rub <aaronr06138@gmail.com>
2023-03-16 12:09:47 -0700

    ca

--- yupyup.py
+++ yupyup.py
@@ -1,10 +1,9 @@
-# import pandas as pd
+import pandas as pd
 
-# grades_df = pd.read_csv('grades.csv')
+grades_df = pd.read_csv('grades.csv')
+
+print(grades_df)
+
+# grades_df['Average'] = grades_df.mean(axis=1)
 
 # print(grades_df)
-
-# # grades_df['Average'] = grades_df.mean(axis=1)
-
-# # print(grades_df)
-print("hello")
