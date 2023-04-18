47863547642ee09c3d0b585f1289050c21bbd5d1
aaron-rub <aaronr06138@gmail.com>
2023-04-18 11:46:00 -0700

    Update hackstestin.py

--- hackstestin.py
+++ hackstestin.py
@@ -1,59 +1,51 @@
 import requests
 import json
-import os.path
-from datetime import datetime, timedelta
-
-# API endpoint and key
+#  endpoint & key
 api_endpoint = "https://openexchangerates.org/api/latest.json"
 api_key = "a6b5d37646474c1b960f881472c93784"
-
-response = requests.get(api_endpoint + "?app_id=" + api_key)
-data = json.loads(response.text)
-currencies = list(data["rates"].keys())
-
-
-cache_file = "exchange_rates.json"
-# Checks if the cache file exists and if it is less then 1 hour in age 
-if os.path.exists(cache_file):
-    with open(cache_file, "r") as f:
-        cache = json.load(f)
-    last_update = datetime.fromisoformat(cache["timestamp"])
-    if datetime.now() - last_update < timedelta(hours=1):
-        rates = cache["rates"]
-else:
-    rates = None
-# Code states that IF the currency conversion chart is not up to date, then fetch new data from the API
-if not rates:
-    response = requests.get(api_endpoint + "?app_id=" + api_key)
-    data = json.loads(response.text)
-    rates = data["rates"]
-
-    # Saves timestamp and current conversion rates to json file
-    with open(cache_file, "w") as f:
-        json.dump({"timestamp": datetime.now().isoformat(), "rates": rates}, f)
-
-print("Here are the available currencies:")
-print(currencies)
-
+# Making list of common currencies to be converted
+valid_currencies = ["USD", "EUR", "GBP", "JPY", "AUD", "CAD", "CHF", "CNY", "HKD"]
+# Prompts user to put in currency that is requested to be coverted and what they wish to be coverted to
 while True:
-    try:
-        amount = float(input("Enter the amount to convert: "))
-    except ValueError:
-        print("Invalid amount. Please enter a valid number.")
+    base_currency = input("Enter the base currency (e.g. USD): ")
+    if base_currency.upper() not in valid_currencies:
+        print("Invalid currency code. Please try again.")
         continue
-    source_currency = input("Enter the source currency code (e.g. USD): ").upper()
-    if not source_currency or source_currency not in rates:
-        print("Invalid source currency code. Please enter a valid code.")
-        continue
-    target_currency = input("Enter the target currency code (e.g. EUR): ").upper()
-    if not target_currency or target_currency not in rates:
-        print("Invalid target currency code. Please enter a valid code.")
+    break
+    
+while True:
+    desired_currency = input("Enter the desired currency (e.g. EUR): ")
+    if desired_currency.upper() not in valid_currencies:
+        print("Invalid currency code. Please try again.")
         continue
     break
 
-# Conversion from source to target currency
-source_rate = rates[source_currency]
-target_rate = rates[target_currency]
-target_amount = amount * target_rate / source_rate
+# API req & store response
+response = requests.get(api_endpoint + "?app_id=" + api_key)
+data = json.loads(response.text)
 
-print("{:.2f} {} is equal to {:.2f} {}".format(amount, source_currency, target_amount, target_currency))
+# fucntion that coverts the inputed currency
+def convert_currency(base_currency, desired_currency, amount):
+    if base_currency == "USD":
+        exchange_rate = data["rates"][desired_currency]
+        converted_amount = amount * exchange_rate
+        return converted_amount
+    else:
+        base_rate = data["rates"][base_currency]
+        desired_rate = data["rates"][desired_currency]
+        exchange_rate = desired_rate / base_rate
+        converted_amount = amount * exchange_rate
+        return converted_amount
+
+# Ask the amount they want to be coverted
+while True:
+    amount_str = input("Enter the amount to convert: ")
+    try:
+        amount = float(amount_str)
+        break
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+
+# Conversion then output 
+result = convert_currency(base_currency.upper(), desired_currency.upper(), amount)
+print("{:.2f} {} is equal to {:.2f} {}".format(amount, base_currency.upper(), result, desired_currency.upper()))
